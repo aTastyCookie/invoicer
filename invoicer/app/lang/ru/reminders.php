630a741b9c156c38ce3e841837045561346dacae
DigitalCommunity <ant1freezeca@gmail.com>
2014-10-18 21:22:23 +0400

    ru
    start translate

---
+++ invoicer/app/lang/ru/reminders.php
@@ -0,0 +1,22 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Password Reminder Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are the default lines which match reasons
+	| that are given by the password broker for a password update attempt
+	| has failed, such as for an invalid token or invalid new password.
+	|
+	*/
+
+	"password" => "Пароли должны состоять как минимум из шести символов и соответствовать друг другу.",
+
+	"user"     => "Мы не можем найти пользователя с такой электронной почтой.",
+
+	"token"    => "Этот токен для сбраса пароля неверен.",
+
+);
