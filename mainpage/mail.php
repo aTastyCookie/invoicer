454078669299d568b4dff33b96dee3a6ab394cce
aTastyCookie <ant1freezeca@gmail.com>
2014-11-07 03:19:57 -0500

    translate and little fix ;)

--- mainpage/mail.php
+++ mainpage/mail.php
@@ -1,27 +1,22 @@
-
 <?php 
-$to = 'ant1freezeca@gmail.com'; // Put in your email address here
-$subject  = "[HubPay Contact] "; // The default subject. Will appear by default in all messages. Change this if you want.
+$to = 'ant1freezeca@gmail.com';
+$subject  = "[HubPay Contact] ";
 
-// User info (DO NOT EDIT!)
-$name = stripslashes($_REQUEST['name']); // sender's name
-$email = stripslashes($_REQUEST['email']); // sender's email
-$company = stripslashes($_REQUEST['company']); // sender's company
+$name = stripslashes($_REQUEST['name']); 
+$email = stripslashes($_REQUEST['email']); 
+$company = stripslashes($_REQUEST['company']);
 
-// The message you will receive in your mailbox
-// Each parts are commented to help you understand what it does exaclty.
-// YOU DON'T NEED TO EDIT IT BELOW BUT IF YOU DO, DO IT WITH CAUTION!
-$msg  = "From: ".$name."\r\n";  // add sender's name to the message
-$msg .= "E-mail: ".$email."\r\n";  // add sender's email to the message
-$msg .= "Company: ".$company."\r\n";  // add sender's email to the message
-$msg .= "Subject: ".$subject."\r\n\n"; // add subject to the message (optional! It will be displayed in the header anyway)
+$msg  = "From: ".$name."\r\n";
+$msg .= "E-mail: ".$email."\r\n";
+$msg .= "Company: ".$company."\r\n";
+$msg .= "Subject: ".$subject."\r\n\n";
 $msg .= "\r\n\n"; 
 
-$mail = @mail($to, $subject, $msg, "From:".$email);  // This command sends the e-mail to the e-mail address contained in the $to variable
+$mail = @mail($to, $subject, $msg, "From:".$email);
 
 if($mail) {
-	echo 'Your message has been sent, we will be in touch shortly!';  //This is the message that will be shown when the message is successfully send
+	echo 'Ваша заявка была успешно отправлена. Мы свяжемся с вами очень скоро :)';
 } else {
-	echo 'Message could not be sent!';   //This is the message that will be shown when an error occured: the message was not send
+	echo 'Заявка не ушла :( Попробуйте еще раз, пожалуйста.';
 }
 ?>
